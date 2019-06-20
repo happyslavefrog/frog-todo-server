0ffaba4127e64b77ea9452572c121c5d5e4ac3ba
pci2676 <pci2676@gmail.com>
2019-06-20 17:34:01 +0900

    프로젝트 생성

---
+++ src/test/java/com/wm/joonggonara/JoonggonaraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wm.joonggonara;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoonggonaraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
