ba2d1600df149645680b5a2468f1c0d0a9c07d59
chanin <pci2676@gmail.com>
2020-06-10 01:32:06 +0900

    feat : 문서화 경로 추가

---
+++ todo-web/src/main/java/com/frog/todo/config/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.frog.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("docs").setViewName("docs/api-guide.html");
+    }
+}
