906216b69255200b3650a036ce3fd62cfa1361b8
chanin <pci2676@gmail.com>
2020-06-18 09:29:05 +0900

    feat: admin module 추가

---
+++ todo-admin/src/main/java/com/frog/todo/TodoAdminWebApplication.java
@@ -0,0 +1,11 @@
+package com.frog.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAdminWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAdminWebApplication.class, args);
+    }
+}
