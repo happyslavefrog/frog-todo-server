f5f0323d701058a30cff26516c8dede6454b45d6
chanin <pci2676@gmail.com>
2020-06-20 01:11:35 +0900

    feat: purge tailwindcss 적용

--- todo-web/src/front/src/main.js
+++ todo-web/src/front/src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import '@/assets/styles/tailwind.css';
 
 Vue.config.productionTip = false;
 
---
+++ todo-web/src/main/resources/static/precache-manifest.baccd9c1835fc6a19e7439529cc7be0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "4a9ebddb809bfeabae46",
+        "url": "css/app.273ca9f2.css"
+    },
+    {
+        "revision": "82b9c7a5a3f405032b1db71a25f67021",
+        "url": "img/logo.82b9c7a5.png"
+    },
+    {
+        "revision": "66a5a5db5964186a3d1b62ada1b1e32b",
+        "url": "index.html"
+    },
+    {
+        "revision": "4a9ebddb809bfeabae46",
+        "url": "js/App.js"
+    },
+    {
+        "revision": "7aa5ee0b0449e37eeadb",
+        "url": "js/about.c1299b2e.js"
+    },
+    {
+        "revision": "f9a7078613e7dd666888",
+        "url": "js/chunk-vendors.0f4403ed.js"
+    },
+    {
+        "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+        "url": "manifest.json"
+    },
+    {
+        "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+        "url": "robots.txt"
+    }
+]);
