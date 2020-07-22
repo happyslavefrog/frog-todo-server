e5bfe3d55bf8ad7610f855c5e2c57674176032c4
chanin <pci2676@gmail.com>
2020-07-22 19:44:36 +0900

    jenkins test

---
+++ todo-web/src/main/resources/static/precache-manifest.5766e7c71b7213ffc7076561f5daa24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fc16df34cc66281ba1",
+    "url": "css/app.0509322f.css"
+  },
+  {
+    "revision": "29e7b7227b32c8ef390aeb53687f571d",
+    "url": "index.html"
+  },
+  {
+    "revision": "15fc16df34cc66281ba1",
+    "url": "js/App.js"
+  },
+  {
+    "revision": "75459302b71bd3b80bb7",
+    "url": "js/chunk-vendors.69d7427c.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
