878d1b3c5beccdb68aee43eba5785e7df0c28da6
chanin <pci2676@gmail.com>
2020-07-23 17:23:29 +0900

    feat: submodule 추가

--- todo-web/src/main/java/com/frog/todo/TodoWebApplication.java
+++ todo-web/src/main/java/com/frog/todo/TodoWebApplication.java
@@ -1,13 +1,23 @@
 package com.frog.todo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TodoWebApplication {
+    public static final String APPLICATION_LOCATIONS =
+            "spring.config.location="
+                    + "classpath:application.yml,"
+                    + "classpath:./config/application-secret.yml";
+    @Value("${frog.key}")
+    private String key;
 
     public static void main(String[] args) {
-        SpringApplication.run(TodoWebApplication.class, args);
+        new SpringApplicationBuilder(TodoWebApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
+
 
 }
